db84f81b7c8f1ed088c3719ed580c6d97bf90628
Lamia Ewais <lamia5098@gmail.com>
2020-11-23 11:13:42 +0300

    define webview variable and set it as ViewContent

--- app/src/main/java/com/bignerdranch/android/nerdfinder/controller/AuthenticationActivity.kt
+++ app/src/main/java/com/bignerdranch/android/nerdfinder/controller/AuthenticationActivity.kt
@@ -3,12 +3,15 @@ package com.bignerdranch.android.nerdfinder.controller
 import android.content.Context
 import android.content.Intent
 import android.os.Bundle
+import android.webkit.WebView
 import androidx.appcompat.app.AppCompatActivity
 
 class AuthenticationActivity : AppCompatActivity() {
-
+    private lateinit var webView: WebView
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        webView = WebView(this)
+        setContentView(webView)
     }
 
     companion object {
